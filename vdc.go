74e77e99f09d448bb854b883305277a61ec194b8
thatmattlove <matt@stellar.tech>
2024-06-07 12:50:07 -0400

    switch usage calculation from allocation to usage

--- vdc.go
+++ vdc.go
@@ -21,10 +21,10 @@ type VDC struct {
 // VDCs is a slice of vcdusage.VDC wrappers.
 type VDCs []VDC
 
-// CoreCount retrieves the allocated CPU MHz for a VDC and calculates the number of cores allocated
-// to all VDCs by dividing the total allocated CPU MHz by the CPU speed.
+// CoreCount retrieves the used CPU MHz for a VDC and calculates the number of cores used
+// by all VDCs by dividing the total used CPU MHz by the CPU speed.
 //
-// For example, if the speed is 3.1 GHz and the allocation amount is 49.6, the core count is 16.
+// For example, if the speed is 3.1 GHz and the used amount is 49.6, the core count is 16.
 func (vdcs VDCs) CoreCount() uint64 {
 	vdcSlice := rill.FromSlice(vdcs, nil)
 	count := uint64(0)
@@ -35,7 +35,7 @@ func (vdcs VDCs) CoreCount() uint64 {
 	return count
 }
 
-// Memory retrieves the amount of allocated memory to all VDCs, represented as a DataStorage type.
+// Memory retrieves the amount of used memory to all VDCs, represented as a DataStorage type.
 func (vdcs VDCs) Memory() DataStorage {
 	vdcSlice := rill.FromSlice(vdcs, nil)
 	mem := DataStorage(0)
@@ -46,7 +46,7 @@ func (vdcs VDCs) Memory() DataStorage {
 	return mem
 }
 
-// Memory retrieves the amount of allocated storage to all VDCs, represented as a DataStorage type.
+// Memory retrieves the amount of used storage to all VDCs, represented as a DataStorage type.
 func (vdcs VDCs) Storage() DataStorage {
 	vdcSlice := rill.FromSlice(vdcs, nil)
 	stor := DataStorage(0)
@@ -94,10 +94,10 @@ func (vdc *VDC) Speed() uint64 {
 	return uint64(*avdc.AdminVdc.VCpuInMhz2)
 }
 
-// CoreCount retrieves the allocated CPU MHz for a VDC and calculates the number of cores allocated
-// to the VDC by dividing the total allocated CPU MHz by the CPU speed.
+// CoreCount retrieves the used CPU MHz for a VDC and calculates the number of cores used
+// by the VDC by dividing the total used CPU MHz by the CPU speed.
 //
-// For example, if the speed is 3.1 GHz and the allocation amount is 49.6, the core count is 16.
+// For example, if the speed is 3.1 GHz and the used amount is 49.6, the core count is 16.
 func (vdc *VDC) CoreCount() uint64 {
 	speed := vdc.Speed()
 	if speed == 0 {
@@ -109,13 +109,13 @@ func (vdc *VDC) CoreCount() uint64 {
 		return 0
 	}
 	for _, capacity := range avdc.AdminVdc.ComputeCapacity {
-		c := uint64(capacity.CPU.Allocated)
+		c := uint64(capacity.CPU.Used)
 		cores += c
 	}
 	return cores / speed
 }
 
-// Memory retrieves the amount of allocated memory to an oVDC, represented as a DataStorage type.
+// Memory retrieves the amount of used memory to an oVDC, represented as a DataStorage type.
 func (vdc *VDC) Memory() DataStorage {
 	avdc, err := vdc.AdminOrg.GetAdminVDCById(vdc.Obj.Vdc.ID, false)
 	if err != nil {
@@ -125,21 +125,21 @@ func (vdc *VDC) Memory() DataStorage {
 	for _, capacity := range avdc.AdminVdc.ComputeCapacity {
 		switch capacity.Memory.Units {
 		case "KB":
-			bm += float64(capacity.Memory.Allocated * kb)
+			bm += float64(capacity.Memory.Used * kb)
 		case "MB":
-			bm += float64(capacity.Memory.Allocated * mb)
+			bm += float64(capacity.Memory.Used * mb)
 		case "GB":
-			bm += float64(capacity.Memory.Allocated * gb)
+			bm += float64(capacity.Memory.Used * gb)
 		case "TB":
-			bm += float64(capacity.Memory.Allocated * tb)
+			bm += float64(capacity.Memory.Used * tb)
 		default:
-			bm += float64(capacity.Memory.Allocated)
+			bm += float64(capacity.Memory.Used)
 		}
 	}
 	return DataStorage(bm)
 }
 
-// Storage retrieves the total amount of allocated storage for an oVDC. If multiple storage
+// Storage retrieves the total amount of used storage for an oVDC. If multiple storage
 // polices are attached an oVDC, the amount will include the sum of all storage policies.
 func (vdc *VDC) Storage() DataStorage {
 	avdc, err := vdc.AdminOrg.GetAdminVDCById(vdc.Obj.Vdc.ID, false)
@@ -152,7 +152,7 @@ func (vdc *VDC) Storage() DataStorage {
 		if err != nil {
 			return 0
 		}
-		sb := sp.StorageTotalMB * mb
+		sb := sp.StorageUsedMB * mb
 		bs += float64(sb)
 	}
 	return DataStorage(bs)
